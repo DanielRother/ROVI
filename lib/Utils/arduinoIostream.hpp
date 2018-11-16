ab621f52e1844496bde01d0288ea92c94696dd59
Daniel Rother <daniel_rother@gmx.de>
2018-11-16 20:59:42 +0100

    Nano-Lantern added

--- lib/Utils/arduinoIostream.hpp
+++ lib/Utils/arduinoIostream.hpp
@@ -1,6 +1,9 @@
 #ifndef __ARDUINOIOSTREAM_H__
 #define __ARDUINOIOSTREAM_H__
 
+#include <Arduino.h>
+#include <string>
+
 template<class T> inline Print &operator <<(Print &obj, T arg) { obj.print(arg); return obj; } 
 inline Print &operator <<(Print &obj, std::string arg) { obj.print(arg.c_str()); return obj; } 
 
