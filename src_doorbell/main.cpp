a79696dc9a7db4e385d2e828a37c37b66545ad13
Daniel Rother <daniel_rother@gmx.de>
2018-11-24 20:16:01 +0100

    DFPlayer for doorbell working again :)

--- src_doorbell/main.cpp
+++ src_doorbell/main.cpp
@@ -9,7 +9,7 @@
 #include <prettyprint.hpp>
 #include <ArduinoIostream.hpp>
 
-#include <ArduinoOTA.h>
+#include "DFRobotDFPlayerMini.h"
 
 //***************************************************************************//
 // Basecamp and Rovi initialize 
@@ -19,36 +19,26 @@
 // Basecamp iot{
 //   Basecamp::SetupModeWifiEncryption::secured, Basecamp::ConfigurationUI::always
 // };
-
-// #include "BaseClasses/RoviDevice.hpp"
-// #include "LED/LEDComponent.hpp"
-// #include "LED/NeoPixelComponent.hpp"
-
 // RoviDevice myRovi(iot);
 
-// LED
-// uint16_t nbNeoPixelLEDs = 20;
-// uint8_t  neoPixelPin    = 15;
+HardwareSerial mySoftwareSerial(1);
+DFRobotDFPlayerMini myDFPlayer;
+uint8_t pinPlayerTX = 12;
+uint8_t pinPlayerRX = 13;
+void printDetail(uint8_t type, int value);
 
-// std::shared_ptr<NeoPixelComponent> leds;
 //***************************************************************************//
 // Arduino setup()
 //***************************************************************************//
 void setup() {
+  Serial.begin(115200);
+  mySoftwareSerial.begin(9600, SERIAL_8N1, pinPlayerRX, pinPlayerTX); // speed, type, RX, TX
+ 
   Serial << "--- setup() ---" << endl;
 
 //   sleep(5);
 //   iot.begin();
 
-//   myRovi.setupRovi();
-
-  // Setup LED
-//   leds = std::make_shared<NeoPixelComponent>(nbNeoPixelLEDs, neoPixelPin);
-//   leds->setSwapRGValues(swapRGValues);
-
-
-
-//   myRovi.addComponent(leds);
 
 //   Serial << "myRovi.baseTopic" << myRovi.getBaseTopic() << endl;
 
@@ -60,13 +50,21 @@ void setup() {
 //   iot.mqtt.onPublish([&](uint16_t packetId) {myRovi.mqttPublished(packetId);});
 
 
-//   // Activate Over-the-Air updates
-//   String otaPassword = iot.configuration.get("OTAPassword");
-//   Serial << "*******************************************" << endl
-//          << "* OTA PASSWORD:" <<  otaPassword << endl
-//          << "*******************************************" << endl;
-//   ArduinoOTA.setPassword(otaPassword.c_str());
-//   ArduinoOTA.begin();
+  
+  Serial.println();
+  Serial.println(F("DFRobot DFPlayer Mini Demo"));
+  Serial.println(F("Initializing DFPlayer ... (May take 3~5 seconds)"));
+  
+  if (!myDFPlayer.begin(mySoftwareSerial)) {  //Use softwareSerial to communicate with mp3.
+    Serial.println(F("Unable to begin:"));
+    Serial.println(F("1.Please recheck the connection!"));
+    Serial.println(F("2.Please insert the SD card!"));
+    while(true);
+  }
+  Serial.println(F("DFPlayer Mini online."));
+  
+  myDFPlayer.volume(10);  //Set volume value. From 0 to 30
+  myDFPlayer.play(1);  //Play the first mp3
 }
 
 
@@ -74,7 +72,6 @@ void setup() {
 // Arduino loop()
 //***************************************************************************//
 void loop() {
-//   ArduinoOTA.handle();
 
 }
 
