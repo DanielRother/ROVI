b4399a45faddbc5e966dad9d1b0f89ff7e3bdfb7
Daniel Rother <daniel_rother@gmx.de>
2021-02-05 22:21:45 +0100

    Cleanup mqtt class structure

---
+++ lib/ROVI/src/Common/MqttConnection.hpp
@@ -0,0 +1,156 @@
+#ifndef __ROVI_MQTT_CONNECTION_H__
+#define __ROVI_MQTT_CONNECTION_H__
+
+#include <AsyncMqttClient.h>
+#include <WiFi.h>
+
+#include <list>
+
+#include <Config/RoviWiFiManager.hpp>
+
+namespace Rovi {
+namespace Common {
+
+class MqttConnection {
+public:
+  using OnConnectCallback = std::function<void(bool sessionPresent)>;
+  using OnDisconnectCallback =
+      std::function<void(AsyncMqttClientDisconnectReason reason)>;
+  using OnSubscribeCallback =
+      std::function<void(uint16_t packetId, uint8_t qos)>;
+  using OnUnsubscribeCallback = std::function<void(uint16_t packetId)>;
+  using OnMessageCallback = std::function<void(
+      char *topic, char *payload, AsyncMqttClientMessageProperties properties,
+      size_t len, size_t index, size_t total)>;
+  using OnPublishCallback = std::function<void(uint16_t packetId)>;
+
+  // TODO
+  // enum class QoS {
+  // }
+
+  // MqttConnection(){};
+  void start(Config::RoviWiFiManager &rwm) /*: rwm{rwm}*/ {
+    Serial.println();
+    Serial.println();
+
+    for (auto c : onConnectCalbacks) {
+      mqtt.onConnect(c);
+    }
+    for (auto c : onDisconnectCallbacks) {
+      mqtt.onDisconnect(c);
+    }
+    for (auto c : onSubscribeCallbacks) {
+      mqtt.onSubscribe(c);
+    }
+    for (auto c : onUnsubscribeCallbacks) {
+      mqtt.onUnsubscribe(c);
+    }
+    for (auto c : onMessageCallbacks) {
+      mqtt.onMessage(c);
+    }
+    for (auto c : onPublishCallbacks) {
+      mqtt.onPublish(c);
+    }
+
+    Serial.println("Set mqtt server config:");
+    Serial.println(rwm.mqttConfig.server.c_str());
+    Serial.println(rwm.mqttConfig.port);
+    Serial.println("Set mqtt server credentials:");
+    Serial.println(rwm.mqttConfig.user.c_str());
+    Serial.println(rwm.mqttConfig.password.c_str());
+
+    strcpy(server, rwm.mqttConfig.server.c_str());
+    port = rwm.mqttConfig.port;
+    strcpy(user, rwm.mqttConfig.user.c_str());
+    strcpy(password, rwm.mqttConfig.password.c_str());
+
+    mqtt.setServer(server, port);
+    mqtt.setCredentials(user, password);
+
+    connect();
+  }
+
+  void addOnConnectCallback(OnConnectCallback callback) {
+    onConnectCalbacks.push_back(callback);
+  }
+  void addOnDisconnectCallback(OnDisconnectCallback callback) {
+    onDisconnectCallbacks.push_back(callback);
+  }
+  void addOnSubscribeCallback(OnSubscribeCallback callback) {
+    onSubscribeCallbacks.push_back(callback);
+  }
+  void addOnUnsubscribeCallback(OnUnsubscribeCallback callback) {
+    onUnsubscribeCallbacks.push_back(callback);
+  }
+  void addOnMessageCallback(OnMessageCallback callback) {
+    onMessageCallbacks.push_back(callback);
+  }
+  void addOnPublishCallback(OnPublishCallback callback) {
+    onPublishCallbacks.push_back(callback);
+  }
+
+  bool connected() const { return mqtt.connected(); }
+  void connect() {
+    Serial.println("Connecting to MQTT...");
+
+    // From Bascamp
+    bool mqttIsConnecting = false;
+
+    // TODO: What is the sense behind these magics?
+    // If the MQTT client is not connected force a disconnect.
+    if (mqtt.connected() != 1) {
+      mqttIsConnecting = false;
+      mqtt.disconnect(true);
+    }
+    // If the MQTT client is not connecting, not already connected and the WiFi
+    // has a connection, try to connect
+    if (!mqttIsConnecting) {
+      if (mqtt.connected() != 1) {
+        if (WiFi.status() == WL_CONNECTED) {
+          mqtt.connect();
+          mqttIsConnecting = true;
+        } else {
+          mqtt.disconnect();
+        }
+      }
+    }
+  }
+
+  void disconnect(bool force = false) { mqtt.disconnect(force); }
+
+  // TODO: Use enum instead of plain int
+  uint16_t subscribe(const std::string &topic, uint8_t qos = 0) {
+    return mqtt.subscribe(topic.c_str(), qos);
+  }
+  uint16_t unsubscribe(const std::string &topic) {
+    mqtt.unsubscribe(topic.c_str());
+  }
+
+  uint16_t publish(const std::string &topic, uint8_t qos, bool retain,
+                   const std::string &payload) {
+    return mqtt.publish(topic.c_str(), qos, retain, payload.c_str(),
+                        payload.size());
+  }
+
+protected:
+  AsyncMqttClient mqtt;
+
+  std::list<OnConnectCallback> onConnectCalbacks;
+  std::list<OnDisconnectCallback> onDisconnectCallbacks;
+  std::list<OnSubscribeCallback> onSubscribeCallbacks;
+  std::list<OnUnsubscribeCallback> onUnsubscribeCallbacks;
+  std::list<OnMessageCallback> onMessageCallbacks;
+  std::list<OnPublishCallback> onPublishCallbacks;
+
+  // Not sure why but the stupid old cstrings require a local copy. Otherwise
+  // something seems to go out of scope or the pointer are for some other
+  // fucking reason invalid.
+  static const int maxCStringLength = 40;
+  char server[maxCStringLength];
+  int port;
+  char user[maxCStringLength];
+  char password[maxCStringLength];
+};
+} // namespace Common
+} // namespace Rovi
+#endif /* __ROVI_MQTT_CONNECTION_H__ */
--- src_basecampAlternative/main.cpp
+++ src_basecampAlternative/main.cpp
@@ -1,14 +1,111 @@
 #include <Arduino.h>
+#include <WiFi.h>
 
 #include <ArduinoIostream.hpp>
 
+#include <functional>
 #include <iostream>
 #include <string>
 
-#include <Common/MqttTest.hpp>
+#include <Common/MqttConnection.hpp>
 #include <Config/RoviWiFiManager.hpp>
 
-Rovi::Common::MqttTest mqttClient;
+class SomeMqttTestClass {
+public:
+  SomeMqttTestClass(Rovi::Common::MqttConnection &mqtt) : mqtt{mqtt} {};
+
+  //   // void WiFiEvent(WiFiEvent_t event) {
+  //   //   Serial.printf("[WiFi-event] event: %d\n", event);
+  //   //   switch (event) {
+  //   //   case SYSTEM_EVENT_STA_GOT_IP:
+  //   //     Serial.println("WiFi connected");
+  //   //     Serial.println("IP address: ");
+  //   //     Serial.println(WiFi.localIP());
+  //   //     connectToMqtt();
+  //   //     break;
+  //   //   case SYSTEM_EVENT_STA_DISCONNECTED:
+  //   //     Serial.println("WiFi lost connection");
+  //   //     //   xTimerStop(
+  //   //     //       mqttReconnectTimer,
+  //   //     //       0); // ensure we don't reconnect to MQTT while
+  //   //     reconnecting to
+  //   //     //       Wi-Fi
+  //   //     //   xTimerStart(wifiReconnectTimer, 0);
+  //   //     break;
+  //   //   }
+  //   // }
+
+  void onMqttConnect(bool sessionPresent) {
+    Serial.println("Connected to MQTT.");
+    Serial.print("Session present: ");
+    Serial.println(sessionPresent);
+    uint16_t packetIdSub = mqtt.subscribe("test/lol", 2);
+    Serial.print("Subscribing at QoS 2, packetId: ");
+    Serial.println(packetIdSub);
+    mqtt.publish("test/lol", 0, true, "test 1");
+    Serial.println("Publishing at QoS 0");
+    uint16_t packetIdPub1 = mqtt.publish("test/lol", 1, true, "test 2");
+    Serial.print("Publishing at QoS 1, packetId: ");
+    Serial.println(packetIdPub1);
+    uint16_t packetIdPub2 = mqtt.publish("test/lol", 2, true, "test 3");
+    Serial.print("Publishing at QoS 2, packetId: ");
+    Serial.println(packetIdPub2);
+  }
+
+  void onMqttDisconnect(AsyncMqttClientDisconnectReason reason) {
+    Serial.println("Disconnected from MQTT.");
+
+    if (WiFi.isConnected()) {
+      //   xTimerStart(mqttReconnectTimer, 0);
+    }
+  }
+
+  void onMqttSubscribe(uint16_t packetId, uint8_t qos) {
+    Serial.println("Subscribe acknowledged.");
+    Serial.print("  packetId: ");
+    Serial.println(packetId);
+    Serial.print("  qos: ");
+    Serial.println(qos);
+  }
+
+  void onMqttUnsubscribe(uint16_t packetId) {
+    Serial.println("Unsubscribe acknowledged.");
+    Serial.print("  packetId: ");
+    Serial.println(packetId);
+  }
+
+  void onMqttMessage(char *topic, char *payload,
+                     AsyncMqttClientMessageProperties properties, size_t len,
+                     size_t index, size_t total) {
+    Serial.println("Publish received.");
+    Serial.print("  topic: ");
+    Serial.println(topic);
+    Serial.print("  qos: ");
+    Serial.println(properties.qos);
+    Serial.print("  dup: ");
+    Serial.println(properties.dup);
+    Serial.print("  retain: ");
+    Serial.println(properties.retain);
+    Serial.print("  len: ");
+    Serial.println(len);
+    Serial.print("  index: ");
+    Serial.println(index);
+    Serial.print("  total: ");
+    Serial.println(total);
+  }
+
+  void onMqttPublish(uint16_t packetId) {
+    Serial.println("Publish acknowledged.");
+    Serial.print("  packetId: ");
+    Serial.println(packetId);
+  }
+
+protected:
+  Rovi::Common::MqttConnection &mqtt;
+};
+
+Rovi::Common::MqttConnection mqttConnection;
+SomeMqttTestClass mqttTest(mqttConnection);
 
 void setup() {
   // put your setup code here, to run once:
@@ -16,7 +113,24 @@ void setup() {
   Serial.println();
 
   auto rwm = Rovi::Config::RoviWiFiManager();
-  mqttClient.start(rwm);
+
+  mqttConnection.addOnConnectCallback(std::bind(
+      &SomeMqttTestClass::onMqttConnect, mqttTest, std::placeholders::_1));
+  mqttConnection.addOnDisconnectCallback(std::bind(
+      &SomeMqttTestClass::onMqttDisconnect, mqttTest, std::placeholders::_1));
+  mqttConnection.addOnSubscribeCallback(
+      std::bind(&SomeMqttTestClass::onMqttSubscribe, mqttTest,
+                std::placeholders::_1, std::placeholders::_2));
+  mqttConnection.addOnUnsubscribeCallback(std::bind(
+      &SomeMqttTestClass::onMqttUnsubscribe, mqttTest, std::placeholders::_1));
+  mqttConnection.addOnMessageCallback(std::bind(
+      &SomeMqttTestClass::onMqttMessage, mqttTest, std::placeholders::_1,
+      std::placeholders::_2, std::placeholders::_3, std::placeholders::_4,
+      std::placeholders::_5, std::placeholders::_6));
+  mqttConnection.addOnPublishCallback(std::bind(
+      &SomeMqttTestClass::onMqttPublish, mqttTest, std::placeholders::_1));
+
+  mqttConnection.start(rwm);
 }
 
 auto start = millis();
