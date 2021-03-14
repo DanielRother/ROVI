d07fd6c3b6f2956904bed0b4580fa79c733b652e
Daniel Rother <daniel_rother@gmx.de>
2021-03-14 22:46:19 +0100

    Update nativity-scene to rovi basecamp

--- src_nativity-scene/main.cpp
+++ src_nativity-scene/main.cpp
@@ -3,94 +3,97 @@
 #include <Arduino.h>
 #include <ArduinoOTA.h>
 
-//***************************************************************************//
-// Basecamp and Rovi initialize 
-//***************************************************************************//
-#include <Basecamp.hpp>
-// Basecamp wird angewiesen einen verschlüsselten Acess-Point zu öffnen. Das Passwort erscheint in der seriellen Konsole.
-Basecamp iot{
-  Basecamp::SetupModeWifiEncryption::secured, Basecamp::ConfigurationUI::always
-};
-
-#include <Devices/LedDeviceMQTT.hpp>
+#include <Common/LED/ColorTypes.h>
+#include <Common/MqttAdapter.hpp>
+#include <Common/MqttConnection.hpp>
+#include <Common/WifiEventHandler.hpp>
 #include <Components/Output/FastLedComponent.hpp>
 #include <Components/Output/NeoPixelComponent.hpp>
-#include <Components/Output/RGBLEDComponent.hpp>
-
-#include<Common/LED/ColorTypes.h>
+#include <Config/RoviWiFiManager.hpp>
+#include <Devices/AbsolutHue.hpp>
 
 const auto nbPixel = 27;
 const auto pin = 13;
 auto name = "krippenbild";
 auto colorCircleDelay = 500;
 
-#ifndef USE_MQTT
-	std::shared_ptr<Rovi::Devices::SimpleLedDevice<Rovi::Components::FastLedComponent<pin, nbPixel>>> xmastree; 
-#else
-	std::shared_ptr<Rovi::Devices::LedDeviceMQTT<Rovi::Components::FastLedComponent<pin, nbPixel>>> xmastree; 
-#endif
+std::shared_ptr<
+    Rovi::Devices::LedDevice<Rovi::Components::FastLedComponent<pin, nbPixel>>>
+    xmastree;
+Rovi::Common::MqttConnection mqttConnection;
+std::shared_ptr<Rovi::Common::MqttAdapter> mqttAdapter;
 
-void setup()
-{
-  std::cout << "--- setup() ---" << endl;
-  sleep(5);
-  iot.begin();
-  std::cout << "*********************************" << std::endl;
-  std::cout << "WiFi SSID" << iot.configuration.get("WifiEssid").c_str() << "; PW: " << iot.configuration.get("WifiPassword").c_str() << std::endl;
-  std::cout << "*********************************" << std::endl;
-	iot.preferences.putUInt("bootcounter", 0);
-	
-  auto leds = std::make_shared<Rovi::Components::FastLedComponent<pin, nbPixel>>();
-//   auto leds = std::make_shared<Rovi::Components::NeoPixelComponent>(pin, nbPixel);
+void setup() {
+  Serial.begin(115200);
+  WiFi.onEvent(wiFiEventHandler);
 
-    auto swapRGValues = std::vector<uint32_t>(nbPixel, 0);
-    for(size_t pixelIdx = 0; pixelIdx < nbPixel; ++pixelIdx) {
-      swapRGValues[pixelIdx] = 1;
-    }
-    leds->setSwapRGValues(swapRGValues);
+  auto rwm = Rovi::Config::RoviWiFiManager();
+
+  auto leds =
+      std::make_shared<Rovi::Components::FastLedComponent<pin, nbPixel>>();
+  //   auto leds = std::make_shared<Rovi::Components::NeoPixelComponent>(pin,
+  //   nbPixel);
+
+  auto swapRGValues = std::vector<uint32_t>(nbPixel, 0);
+  for (size_t pixelIdx = 0; pixelIdx < nbPixel; ++pixelIdx) {
+    swapRGValues[pixelIdx] = 1;
+  }
+  leds->setSwapRGValues(swapRGValues);
 
   auto effects = std::vector<std::shared_ptr<Rovi::LEDEffect>>();
-  effects.push_back(Rovi::LEDEffectFactory::getEffect("color_gradient", leds.get()));
-  effects.push_back(Rovi::LEDEffectFactory::getEffect("white_static", leds.get()));
-  effects.push_back(Rovi::LEDEffectFactory::getEffect("color_static", leds.get()));
-  effects.push_back(Rovi::LEDEffectFactory::getEffect("color_flow", leds.get()));
+  effects.push_back(
+      Rovi::LEDEffectFactory::getEffect("color_gradient", leds.get()));
+  effects.push_back(
+      Rovi::LEDEffectFactory::getEffect("white_static", leds.get()));
+  effects.push_back(
+      Rovi::LEDEffectFactory::getEffect("color_static", leds.get()));
+  effects.push_back(
+      Rovi::LEDEffectFactory::getEffect("color_flow", leds.get()));
   effects.push_back(Rovi::LEDEffectFactory::getEffect("rainbow", leds.get()));
-  effects.push_back(Rovi::LEDEffectFactory::getEffect("color_circle_rgb", leds.get()));
-  effects.push_back(Rovi::LEDEffectFactory::getEffect("color_circle_red", leds.get()));
-  effects.push_back(Rovi::LEDEffectFactory::getEffect("color_circle_blue", leds.get()));
-  effects.push_back(Rovi::LEDEffectFactory::getEffect("color_circle_green", leds.get()));
-  effects.push_back(Rovi::LEDEffectFactory::getEffect("color_circle_purple", leds.get()));
-  effects.push_back(Rovi::LEDEffectFactory::getEffect("running_onoff_single", leds.get()));
-  effects.push_back(Rovi::LEDEffectFactory::getEffect("running_onoff_multi", leds.get()));
-  effects.push_back(Rovi::LEDEffectFactory::getEffect("running_onoff_twocolor", leds.get()));
-  effects.push_back(Rovi::LEDEffectFactory::getEffect("running_onoff_rainbow", leds.get()));
-  effects.push_back(Rovi::LEDEffectFactory::getEffect("running_onoff_single_random", leds.get()));
-  effects.push_back(Rovi::LEDEffectFactory::getEffect("running_onoff_multi_random", leds.get()));
-  effects.push_back(Rovi::LEDEffectFactory::getEffect("running_onoff_twocolor_zufall", leds.get()));
-  effects.push_back(Rovi::LEDEffectFactory::getEffect("running_onoff_rainbow_random", leds.get()));
-
-
-#ifndef USE_MQTT
-  xmastree = std::make_shared<Rovi::Devices::SimpleLedDevice<Rovi::Components::FastLedComponent<pin, nbPixel>>>(iot, leds, effects, name);
-#else
-  xmastree = std::make_shared<Rovi::Devices::LedDeviceMQTT<Rovi::Components::FastLedComponent<pin, nbPixel>>>(iot, leds, effects, name);
-#endif
+  effects.push_back(
+      Rovi::LEDEffectFactory::getEffect("color_circle_rgb", leds.get()));
+  effects.push_back(
+      Rovi::LEDEffectFactory::getEffect("color_circle_red", leds.get()));
+  effects.push_back(
+      Rovi::LEDEffectFactory::getEffect("color_circle_blue", leds.get()));
+  effects.push_back(
+      Rovi::LEDEffectFactory::getEffect("color_circle_green", leds.get()));
+  effects.push_back(
+      Rovi::LEDEffectFactory::getEffect("color_circle_purple", leds.get()));
+  effects.push_back(
+      Rovi::LEDEffectFactory::getEffect("running_onoff_single", leds.get()));
+  effects.push_back(
+      Rovi::LEDEffectFactory::getEffect("running_onoff_multi", leds.get()));
+  effects.push_back(
+      Rovi::LEDEffectFactory::getEffect("running_onoff_twocolor", leds.get()));
+  effects.push_back(
+      Rovi::LEDEffectFactory::getEffect("running_onoff_rainbow", leds.get()));
+  effects.push_back(Rovi::LEDEffectFactory::getEffect(
+      "running_onoff_single_random", leds.get()));
+  effects.push_back(Rovi::LEDEffectFactory::getEffect(
+      "running_onoff_multi_random", leds.get()));
+  effects.push_back(Rovi::LEDEffectFactory::getEffect(
+      "running_onoff_twocolor_zufall", leds.get()));
+  effects.push_back(Rovi::LEDEffectFactory::getEffect(
+      "running_onoff_rainbow_random", leds.get()));
 
   // Deactivae automatic effect change
   xmastree->setTimePerEffect(std::chrono::minutes{0});
 
-  // Activate Over-the-Air updates
-  String otaPassword = iot.configuration.get("OTAPassword"); // The prefedined variable OTAPassword is always reset to '1'; TODO: Check why/fix?
-  std::cout << "*******************************************" << endl
-         << "* OTA PASSWORD:" <<  otaPassword.c_str() << endl
-         << "*******************************************" << endl;
+  xmastree = std::make_shared<Rovi::Devices::LedDevice<
+      Rovi::Components::FastLedComponent<pin, nbPixel>>>(leds, effects, name);
 
-  ArduinoOTA.setPassword(otaPassword.c_str());
+  mqttAdapter = std::make_shared<Rovi::Common::MqttAdapter>(
+      *std::static_pointer_cast<Rovi::Devices::BasicDevice>(xmastree),
+      mqttConnection, rwm);
+  mqttConnection.start(rwm);
+
+  ArduinoOTA.setPassword(rwm.otaPassword.c_str());
   ArduinoOTA.begin();
 }
 
-void loop()
-{
-    ArduinoOTA.handle();
-    xmastree->update();
+void loop() {
+  ArduinoOTA.handle();
+  // xmastree->update();    // Only required, if no MQTT adapter is used.
+  mqttAdapter->update();
 }
