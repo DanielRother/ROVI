509bb9c707fa382632d82ec13435e20fd2000f84
Daniel Rother <daniel_rother@gmx.de>
2022-01-08 20:33:00 +0100

    Add audio output

--- src_jukebox/main.cpp
+++ src_jukebox/main.cpp
@@ -1,3 +1,6 @@
+// Ohne WebDAV 84,9%
+// SD + Audio + WebDAV = 87,7%
+
 /*
   SD card test for esp32
 
@@ -14,6 +17,19 @@
 
    https://www.mischianti.org
 */
+
+// My stuff
+#include <algorithm>
+// #include <iostream>
+#include <string>
+#include <vector>
+
+// #include <ArduinoIostream.hpp>
+// #include <prettyprint.hpp>
+
+void getSortedMusicFiles(const std::string dirPath,
+                         std::vector<std::string> &files);
+
 // include the SD library:
 #include <SD.h>
 #include <SPI.h>
@@ -22,6 +38,7 @@
 const int chipSelect = SS;
 
 void printDirectory(File dir, int numTabs);
+void help();
 
 // WebDAV stuff
 #include <ESPWebDAV.h>
@@ -48,6 +65,24 @@ FS &gfs = SD;
 WiFiServer tcp(80);
 
 ESPWebDAV dav;
+
+// Audio Stuff
+#include <Audio.h>
+
+#define SD_CS 5
+#define SPI_MOSI 23
+#define SPI_MISO 19
+#define SPI_SCK 18
+#define I2S_DOUT 25
+#define I2S_BCLK 27
+#define I2S_LRC 26
+
+Audio audio;
+
+auto musicDir = "/Foo Fighters - Medicine At Midnight (2021)";
+std::vector<std::string> musicFiles;
+auto fileIter = musicFiles.begin();
+bool isPaused = false;
 
 void setup() {
   /**********************************
@@ -109,8 +144,17 @@ void setup() {
   //   Serial.print("Used bytes: ");
   //   Serial.println(SD.usedBytes());
 
-  File dir = SD.open("/");
+  File dir = SD.open(musicDir);
   //   printDirectory(dir, 0);
+
+  getSortedMusicFiles(musicDir, musicFiles);
+  for (auto f : musicFiles) {
+    Serial.println(f.c_str());
+  }
+  fileIter = musicFiles.begin();
+  isPaused = false;
+  //   std::cout << "musicFiles: " << musicFiles << std::endl;
+
   Serial.println("SD part is ready");
 
   /**********************************
@@ -149,9 +193,115 @@ void setup() {
   });
 
   Serial.println("WebDAV server started");
+
+  //******* Init Audio *****/
+  Serial.println("Init audio");
+  //   pinMode(SD_CS, OUTPUT);      digitalWrite(SD_CS, HIGH);
+  // SPI.begin(SPI_SCK, SPI_MISO, SPI_MOSI);
+  // Serial.begin(115200);
+  // SD.begin(SD_CS);
+  // WiFi.disconnect();
+  // WiFi.mode(WIFI_STA);
+  // WiFi.begin(ssid.c_str(), password.c_str());
+  // while (WiFi.status() != WL_CONNECTED) delay(1500);
+  audio.setPinout(I2S_BCLK, I2S_LRC, I2S_DOUT);
+  audio.setVolume(21); // 0...21
+
+  Serial.println("Play music");
+  //   audio.connecttoFS(SD, "/01 - Making A Fire.mp3");
+  //    audio.connecttohost("http://www.wdr.de/wdrlive/media/einslive.m3u");
+  //    audio.connecttohost("http://macslons-irish-pub-radio.com/media.asx");
+  audio.connecttohost("http://mp3.ffh.de/radioffh/hqlivestream.aac"); //
+  //    128k aac
+  //   audio.connecttohost(
+  //   "http://mp3.ffh.de/radioffh/hqlivestream.mp3"); //  128k mp3
+  //    audio.connecttohost("https://github.com/schreibfaul1/ESP32-audioI2S/raw/master/additional_info/Testfiles/sample1.m4a");
+  //    // m4a
+  //    audio.connecttohost("https://github.com/schreibfaul1/ESP32-audioI2S/raw/master/additional_info/Testfiles/test_16bit_stereo.wav");
+  //    // wav audio.connecttospeech("Wenn die Hunde schlafen, kann der Wolf gut
+  //    Schafe stehlen.", "de");
+  Serial.println("Music started");
 }
 
-void loop(void) { dav.handleClient(); }
+bool playFiles = false;
+std::string nextFile() {
+  ++fileIter;
+  if (fileIter < musicFiles.end()) {
+    return *fileIter;
+  } else {
+    playFiles = false;
+    return "";
+  }
+}
+
+void loop(void) {
+  dav.handleClient();
+  audio.loop();
+
+  if (playFiles && !audio.isRunning() && !isPaused) {
+    Serial.print("Play next file ");
+    auto file = nextFile();
+    if (file != "") {
+      Serial.println(file.c_str());
+      audio.connecttoFS(SD, file.c_str());
+    } else {
+      Serial.println("No more files");
+      playFiles = false;
+    }
+  }
+
+  int c = Serial.read();
+  if (c != -1) {
+    Serial.print("Read: ");
+    Serial.println(c);
+  }
+  if (c > 0) {
+    if (c == 'F') {
+      Serial.println("formatting...");
+      //   if (FILESYSTEM.format())
+      //     Serial.println("Success");
+      //   else
+      //     Serial.println("Failure");
+    } else if (c == 'D') {
+      Serial.printf(">>>>>>>> dir /\n");
+      dav.dir("/", &Serial);
+      Serial.printf("<<<<<<<< dir\n");
+    } else if (c == 'C') {
+      Serial.println("Create file");
+      auto f = gfs.open("/readme.md", "w");
+      f.printf("hello\n");
+      f.close();
+    } else if (c == 'P') {
+      Serial.println("Pause");
+      audio.pauseResume();
+      isPaused = !isPaused;
+    } else if (c == 'W') {
+      Serial.print("Play web radio ");
+      playFiles = false;
+      isPaused = false;
+      audio.connecttohost("http://mp3.ffh.de/radioffh/hqlivestream.aac"); //
+    } else if (c == 'M') {
+      Serial.println("Play MP3 files ");
+      fileIter = musicFiles.begin();
+      playFiles = true;
+      isPaused = false;
+      audio.connecttoFS(SD, (*fileIter).c_str());
+    } else if (c == 'N') {
+      Serial.print("Play next file ");
+      auto file = nextFile();
+      if (file != "") {
+        Serial.println(file.c_str());
+        playFiles = true;
+        audio.connecttoFS(SD, file.c_str());
+      } else {
+        Serial.println("No more files");
+        playFiles = false;
+      }
+    } else {
+      help();
+    }
+  }
+}
 
 void printDirectory(File dir, int numTabs) {
   while (true) {
@@ -181,6 +331,43 @@ void printDirectory(File dir, int numTabs) {
     }
     entry.close();
   }
+}
+
+bool hasEnding(std::string const &fullString, std::string const &ending) {
+  if (fullString.length() >= ending.length()) {
+    return (0 == fullString.compare(fullString.length() - ending.length(),
+                                    ending.length(), ending));
+  } else {
+    return false;
+  }
+}
+
+void getSortedMusicFiles(const std::string dirPath,
+                         std::vector<std::string> &files) {
+  //   auto files = std::vector<std::string>{};
+
+  auto dir = SD.open(dirPath.c_str());
+
+  while (true) {
+    File entry = dir.openNextFile();
+    if (!entry) {
+      // no more files
+      break;
+    }
+
+    if (entry.isDirectory()) {
+      // Do nothing
+    } else {
+      std::string curFile = entry.name();
+      if (hasEnding(curFile, "mp3") || hasEnding(curFile, "aac") ||
+          hasEnding(curFile, "wav")) {
+        files.push_back(curFile);
+      }
+    }
+    entry.close();
+  }
+
+  std::sort(files.begin(), files.end());
 }
 
 // // Original code
@@ -246,11 +433,11 @@ void printDirectory(File dir, int numTabs) {
 //   Serial.println("WebDAV server started");
 // }
 
-// void help() {
-//   Serial.printf("interactive: F/ormat D/ir C/reateFile\n");
+void help() {
+  Serial.printf("interactive: F/ormat D/ir C/reateFile\n");
 
-//   Serial.printf("Heap stats: free heap: %u\n", ESP.getHeapSize());
-// }
+  Serial.printf("Heap stats: free heap: %u\n", ESP.getHeapSize());
+}
 
 // // ------------------------
 // void loop() {
